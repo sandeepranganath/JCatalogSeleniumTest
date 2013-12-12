3a60962e824bf70f395a3335253ce3c352934545
RLDevOps <sandeep.ranganath@relevancelab.com>
2013-12-12 12:24:48 +0530

    Update MainDriver.java

--- src/com/rl/test/selenium/MainDriver.java
+++ src/com/rl/test/selenium/MainDriver.java
@@ -3,5 +3,6 @@ package com.rl.test.selenium;
 import org.openqa.selenium.WebDriver;
 
 public class MainDriver {
-	static WebDriver driver;
+	//static WebDriver driver;
+	static HtmlUnitDriver driver; 
 }
